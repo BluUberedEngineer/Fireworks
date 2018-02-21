892505654f2039a50116598a612ba26d54f88d6e
David Jonas <davidjonasdesign@gmail.com>
2018-02-21 17:36:56 +0100

    Added the code built in class.

---
+++ Assets/Firework.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firework : MonoBehaviour {
+
+	List<Particle> particles;
+
+	public int numParticles;
+	public float speed;
+	public int maxAge;
+	public GameObject prefab;
+	public float gravity;
+
+	// Use this for initialization
+	void Start () {
+		particles = new List<Particle>();
+		Init();
+	}
+
+
+	public void Init(){
+		for(int i=0; i<numParticles; i++)
+		{
+			particles.Add(new Particle(transform.position, speed + Random.Range(-2f, 2f), maxAge, prefab));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+		Draw();
+
+		if(isFinished())
+		{
+			Init();
+		}
+	}
+
+	public void Move() {
+		for(int i=0; i<particles.Count; i++)
+		{
+			particles[i].Move(Vector3.down * gravity);
+			if(!particles[i].isAlive())
+			{
+				particles.Remove(particles[i]);
+			}
+		}
+	}
+
+	public void Draw() {
+		for(int i=0; i<particles.Count; i++)
+		{
+			particles[i].Draw();
+		}
+	}
+
+	public bool isFinished()
+	{
+		return particles.Count == 0;
+	}
+}
---
+++ Assets/Particle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle {
+	bool active;
+	int age;
+	Vector3 position;
+	Vector3 direction;
+	float speed;
+	int maxAge;
+	GameObject gameObject;
+
+	public Particle(Vector3 position, float speed, int maxAge, GameObject prefab) {
+		active = true;
+		age = 0;
+		this.position = position;
+		this.speed = speed;
+		direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+		direction.Normalize();
+		direction = direction * speed;
+		this.maxAge = maxAge;
+		gameObject = GameObject.Instantiate(prefab, position, Quaternion.identity);
+
+		//Debug.Log("Created particle with speed " + speed + " and direction " + direction.x + ", " + direction.y + ", " + direction.z);
+	}
+
+	public void Move()
+	{
+		if(active)
+		{
+			position = position + direction;
+			age++;
+
+			if(age > maxAge)
+			{
+				Die();
+			}
+		}
+	}
+		
+	public void Move(Vector3 force)
+	{
+		direction = direction + force;
+		Move();
+	}
+
+	public void Draw()
+	{
+		if(active)
+		{
+			gameObject.transform.position = position;
+		}
+	}
+
+	public void Die(){
+		active = false;
+		GameObject.Destroy(gameObject);
+	}
+
+	public bool isAlive()
+	{
+		return active;
+	}
+}
